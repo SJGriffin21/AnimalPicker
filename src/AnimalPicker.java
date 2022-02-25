f222df201ab8f5927f94ee138848a192cb5e7241
SJGriffin21 <sjgriffin@quinnipiac.edu>
2022-02-25 15:37:08 -0500

    Added ASCII art fish

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -8,7 +8,7 @@ public class AnimalPicker {
 		boolean selected = false;
 		
 		while (!selected) {
-			System.out.println("What animal would you like to see, a dog or a cat?");
+			System.out.println("What animal would you like to see, a dog, cat, or a fish?");
 			String response = userInput.nextLine();
 		
 			if (response.equalsIgnoreCase("cat")) {
@@ -17,6 +17,10 @@ public class AnimalPicker {
 			}
 			else if (response.equalsIgnoreCase("dog")) {
 				System.out.println("  __      _\no\'\')}____//\n `_/      )\n (_(_/-(_/");
+				selected = true;
+			}
+			else if (response.equalsIgnoreCase("fish")) {
+				System.out.println("      /`·.¸\n     /¸...¸`:·\n ¸.·´  ¸   `·.¸.·´)\n: © ):´;      ¸  {\n `·.¸ `·  ¸.·´\\`·¸)\n     `\\\\´´\\¸.·´");
 				selected = true;
 			}
 			else {
